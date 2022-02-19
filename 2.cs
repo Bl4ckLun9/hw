e6da4ec42b0604b09332c6765ecd973b0450e557
BlackLung <48220462+Bl4ckLun9@users.noreply.github.com>
2022-02-19 21:40:29 +0400

    Update 2.cs

--- 2.cs
+++ 2.cs
@@ -6,19 +6,39 @@
 	int a3;
 
 	a1 = number / 100;
-	a2 = number / 10; //12
-	a3 = 
+	a2 = (number / 10) % 10;
+	a3 = number % 10;
 }
 
 //2. მოცემულია მთელი ორნიშნა რიცხვი, აღვწეროთ result ცვლადი და მასში მოვათავსოთ მოცემული რიცხვის ციფრების ჯამი.
 {
-  int number = 97;
-  int result;
-  
-  result = 
+	int number = 97;
+	int result;
+	int firstDigit;
+	int lastDigit;
+	
+	firstDigit = number / 10;
+	lastDigit = number % 10;
+  	result = firstDigit + lastDigit;
 }
 
 //3. მოცემულია ოთხნიშნა რიცხვი, აღვწეროთ result ცვლადი და მასში მოვათავსოთ მოცემული რიცხვის ლუწი ციფრების ჯამი.
 {
-  
+	int num = 1234;
+	int even;
+	int result = 0;
+	
+	while (n > 0)
+	{
+		even = n % 10;
+		if (even % 2 == 0)
+		{
+			result = result + even;
+		}
+			
+			n = n / 10;
+	}	
+}
+	
+	
 }
