84b2533df04624e0db2b98588537c3a1ce504492
BlackLung <48220462+Bl4ckLun9@users.noreply.github.com>
2022-02-20 15:07:39 +0400

    Update 2.cs

--- 2.cs
+++ 2.cs
@@ -1,34 +1,30 @@
-{
 //1. მოცემულია სამნიშნა რიცხვი, აღვვწეროთ სამი ცვლადი a1, a2, a3, მოვათავსოთ აღწერილ ცვლადებში რიცხვის ციფრები.
-
-	int Number = 127;
+{
+	int number = 127;
 	int a1;
 	int a2;
 	int a3;
 
-	a1 = Number / 100;
-	a2 = (Number / 10) % 10;
-	a3 = Number % 10;
+	a1 = number / 100;
+	a2 = (number / 10) % 10;
+	a3 = number % 10;
 }
-
-{
 //2. მოცემულია მთელი ორნიშნა რიცხვი, აღვწეროთ result ცვლადი და მასში მოვათავსოთ მოცემული რიცხვის ციფრების ჯამი.
-	int Number = 97;
-	int Result;
-	int FirstDigit;
-	int LastDigit;
-	
-	FirstDigit = Number / 10;
-	LastDigit = Number % 10;
-  	Result = FirstDigit + LastDigit;
-}
-
-
 {
+	int number = 97;
+	int result;
+	int firstDigit;
+	int lastDigit;
+	
+	FirstDigit = number / 10;
+	lastDigit = number % 10;
+  	result = firstDigit + lastDigit;
+}
 //3. მოცემულია ოთხნიშნა რიცხვი, აღვწეროთ result ცვლადი და მასში მოვათავსოთ მოცემული რიცხვის ლუწი ციფრების ჯამი.
-	int Num = 1234;
-	int Even;
-	int Result = 0;
+{
+	int num = 1234;
+	int even;
+	int result = 0;
 	
 	while (num > 0)
 	{
@@ -44,7 +40,7 @@
 
 {
 //იგივე რაც 3. დავალება
-	int Num = 1234;
+	int num = 1234;
 	int Result;
 	int FirstDigit;
 	int SecondDigit;
