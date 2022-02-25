9ab1df88302b6df27154dbe2de6161ca48dcb1bb
BlackLung <48220462+Bl4ckLun9@users.noreply.github.com>
2022-02-25 21:21:27 +0400

    Update 2.cs

--- 2.cs
+++ 2.cs
@@ -1,11 +1,17 @@
-// 1. Modify the given code, so that it only prints even numbers from 1 to 20 included. Don't use % operator.
+// 1. print even numbers from 1 to 20 included. Don't use % operator.
 	
 
-
+ 
 // 2. Take an input from a user, if the string includes a character "a", print True only one time. Don't use any built-in string functions.
-
-
-
+	string str;
+	char ch = 'a';
+	bool result;
+	str = Console.ReadLine();
+	
+	result = str.Contains(ch);
+	
+	
+	Console.WriteLine(result);
 
 // 3. Take a positive integer (x) from a user and print numbers from 1 to x, excluding multiples of 7.
 
