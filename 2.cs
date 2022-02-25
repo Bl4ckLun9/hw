45f6b7b65202755f8c55c2931eab5b70790157f5
BlackLung <48220462+Bl4ckLun9@users.noreply.github.com>
2022-02-25 20:22:31 +0400

    Update 2.cs

--- 2.cs
+++ 2.cs
@@ -1,2 +1,5 @@
 // 1. Modify the given code, so that it only prints even numbers from 1 to 20 included. Don't use % operator.
 	
+
+
+// 2. Take an input from a user, if the string includes a character "a", print True only one time. Don't use any built-in string functions.
