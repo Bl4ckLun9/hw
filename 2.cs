ebc76f9c1495d32729770522e48f379de25c42d0
BlackLung <48220462+Bl4ckLun9@users.noreply.github.com>
2022-02-25 20:22:47 +0400

    Update 2.cs

--- 2.cs
+++ 2.cs
@@ -3,3 +3,8 @@
 
 
 // 2. Take an input from a user, if the string includes a character "a", print True only one time. Don't use any built-in string functions.
+
+
+
+
+// 3. Take a positive integer (x) from a user and print numbers from 1 to x, excluding multiples of 7.
