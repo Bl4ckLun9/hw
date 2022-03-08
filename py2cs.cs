7647f58fa311b4e70a061931966e6983a2cf5350
BlackLung <48220462+Bl4ckLun9@users.noreply.github.com>
2022-03-08 14:19:45 +0400

    Update py2cs.cs

--- py2cs.cs
+++ py2cs.cs
@@ -12,3 +12,70 @@
 
 // 5. Add a third input for age, output should look like this example: "Age of Brad Pitt is 58". Use only one print() function.
 
+
+// 6. Take values of length and width of a rectangle from user and print if it's a square or not. ("It is a square"/"It is not a square")
+
+
+// 7. Take two int values from user and print greatest among them. Use one-line if else only.
+
+
+/* 8. A school has following rules for grading system:
+   a. Below 25 - F
+   b. 25 to 45 - E
+   c. 46 to 50 - D
+   d. 51 to 60 - C
+   e. 61 to 80 - B
+   f. Above 80 - A
+   Ask user to enter their mark and print the corresponding grade.
+*/
+
+/* 9. A shop will give discount of 10% if the cost of purchased quantity is more than 1000.
+   Ask user for quantity
+   Suppose, one unit will cost 100.
+   Judge and print total cost for user. ("Total cost is ...")
+*/
+
+// 10. Write a code, so that it only prints even numbers from 1 to 20 included. Don't use % operator.
+
+// 11. Take an input from a user, if the string includes a character "a", print True only one time. Don't use any built-in string functions.
+
+// 12. Take a positive integer (x) from a user and print numbers from 1 to x, excluding multiples of 7 (7-ის ჯერადების გარდა).
+
+// 13. Take an input from a user, print the amount of "a" character. Don't use any built-in string functions.
+
+// 14. Create a program with a looped input, don't quit until the received input is a number 0.
+
+// 15. Create a program to print a multiplication table of a received input number.
+
+// 16. Take an input from a user and find the factorial of a number.
+
+// 17. Display Fibonacci series up to 10 terms. Note that first two terms are 0 and 1.
+
+// 18. Take an input from a user, check if the number is prime or not. ("It's a prime number/It's not a prime number")
+
+// 19. Take an input from a user, print every prime number from 1 to X.
+
+/* 20. Draw these two pyramids using numbers and capital letters using nested loops:
+            0                                   A
+          1 0 1                                ABA
+        2 1 0 1 2                             ABCBA
+      3 2 1 0 1 2 3                          ABCDCBA
+    4 3 2 1 0 1 2 3 4                       ABCDEDCBA
+  5 4 3 2 1 0 1 2 3 4 5                    ABCDEFEDCBA
+*/
+
+/* 21. Draw this hourglass using nested loops:
+  * * * * *
+   * * * *
+    * * *
+     * *
+      *
+     * *
+    * * *
+   * * * *
+  * * * * *
+*/
+
+// 23. Take an input from a user (if 0, exit), print horizontally alligned spaced * character corresponding to the amount of the divisors of the number. Example, if input is 10: * * * *
+
+
