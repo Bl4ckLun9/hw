1f00695f22791f27d26820fd90e61f78c23a59d2
BlackLung <48220462+Bl4ckLun9@users.noreply.github.com>
2022-03-08 14:09:04 +0400

    Update py2cs.cs

--- py2cs.cs
+++ py2cs.cs
@@ -1,1 +1,14 @@
+// 1. Create an input that takes an integer and outputs "True" for even numbers or "False" for odd numbers. Hint: Result might be an error, try to research "type casting" and fix it.
+
+
+# 2. Create inputs for three numbers, output "True" if first two numbers are equal to or greater than a third number, otherwise output "False".
+
+
+# 3. Create inputs for three numbers, output "True" if any of first two numbers is divisible by a third number without a remainder.
+
+
+# 4. Create two seperate inputs for a First Name and a Last Name, output should look like this example: "Full Name is Brad Pitt". Use only one print() function.
+
+
+# 5. Add a third input for age, output should look like this example: "Age of Brad Pitt is 58". Use only one print() function.
 
